6c084f6d386bfa63965b6607baac71e7fface830
Eivin Adlawan <eadlawa2@gmail.com>
2020-02-18 01:25:02 -0600

    Savepoint: Did my additions in my local copy. Now pushing it into my remote repoand to try and see if the local revisions show up on Codesandbox.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <h3> Hello world! This is a test after a commit</h3>
+      <h3> Hello world! This is a test after a commit.</h3>
+      <h4>Note: To edit from Codesandbox (online editor), I have to use my terminal and do a 'git pull origin master' to pull down the changes from my remote repo and pull it down to my local copy so both copies are synced.
+
+        After I'm done with my local copy revisions, I can use my terminal to do a 'git push origin master'.
+      </h4>
     </div>
   );
 }
